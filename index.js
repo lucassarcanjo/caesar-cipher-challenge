c58b3d92234de92143963c71cbf1f59a2fdff09c
lucassarcanjo <lucassarcanjo@gmail.com>
2020-04-03 18:35:19 -0300

    Correcao de erros de submissao

--- index.js
+++ index.js
@@ -41,66 +41,60 @@ function caesarShift (str, count) {
     return output
 }
 
-async function submitCodenationTest(data_obj, my_token) {
-    function getFormData(object) {
+async function submitCodenationTest(file, my_token) {
+    function getFormData(file) {
         const formData = new FormData();
-        const answer = fs.createReadStream('./answer.json');
+        const answer = fs.createReadStream(file);
         formData.append('answer', answer);
     
         return formData;
     }
 
     const api_url = 'https://api.codenation.dev/v1/challenge/dev-ps/submit-solution';
-    const form = getFormData(data_obj);
+    const form = getFormData(file);
 
-    return await axios.post(api_url, form, {
-        headers: form.getHeaders(),
-        params: {
-            token: my_token
-        }
-    });
+    try {
+        return await axios.post(api_url, form, {
+            headers: form.getHeaders(),
+            params: {
+                token: my_token
+            }
+        });
+    } catch (error) {
+        console.log(`Erro ao tentar executar a submissao de dados na API Codenation: ${error}`);
+    }
 }
 
+async function main() {
+    console.log('Desafio Codenation - Criptografia de Julio Cesar\n\n');
+
+    const my_token = process.env.API_TOKEN;
+    
+    // Obtendo mensagem do server Codenation utilizando meu token pessoal
+    const data = await getCodenationMessage(my_token);
+    console.log(`Dado de entrada (rotação de ${data.numero_casas}):\n${data.cifrado} `)
+    
+    data.decifrado = caesarShift(data.cifrado, -data.numero_casas);
+    console.log(`Dado decifrado: ${data.decifrado}`);
+    
+    
+    // Resumo criptografico com SHA1
+    data.resumo_criptografico = crypto.createHash('sha1').update(data.decifrado).digest('hex');
+    console.log(`Dado encriptado com SHA1: ${data.resumo_criptografico}`);
+    
+    
+    // Salvando o arquivo answer.json
+    fs.writeFileSync('answer.json', JSON.stringify(data, null, 2),
+        (err) => {
+            if(err) 
+                console.log(err);
+    });
+    
+    
+    // Submissão do arquivo atualizado na api da Codenation
+    const score = await submitCodenationTest('./answer.json', my_token);
+    console.log(score.data);
+}
 
 // ----------------------------------------------------------------------------------------------------
-console.log('Desafio Codenation - Criptografia de Julio Cesar\n\n');
-
-const my_token = process.env.API_TOKEN;
-
-// Obtendo mensagem do server Codenation utilizando meu token pessoal
-// const res_data = getCodenationMessage(my_token);
-
-
-// decodificação da mensagem
-const data = {
-    cifrado: 'kbwb jt up kbwbtdsjqu xibu dbs jt up dbsqfu. disjt ifjmnboo',
-    decifrado: '',
-    numero_casas: 1,
-    resumo_criptografico: '',
-    token: process.env.API_TOKEN
-};
-
-console.log(`Dado de entrada (rotação de ${data.numero_casas}):\n${data.cifrado} `)
-
-data.decifrado = caesarShift(data.cifrado, -data.numero_casas);
-console.log(`Dado decifrado: ${data.decifrado}`);
-
-
-// Resumo criptografico com SHA1
-data.resumo_criptografico = crypto.createHash('sha1').update(data.decifrado).digest('hex');
-console.log(`Dado encriptado com SHA1: ${data.resumo_criptografico}`);
-
-
-// Salvando o arquivo answer.json
-const jsonData = JSON.stringify(data, null, 2);
-
-fs.writeFile('answer.json', jsonData, (err) => {
-    if(err) {
-        console.log(err);
-    }
-});
-
-
-// Submissão do arquivo atualizado na api da Codenation - Nao funcional
-// const score = submitCodenationTest(data, my_token);
-console.log(score);
+main();
